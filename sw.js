2e87996da704c824316a8bda2faba7c8c44fc6a8
angelmicelti <angelmicelti@users.noreply.github.com>
2026-04-19 21:43:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ies-mododios-v10.8'; // Actualiza la versión para forzar la activación
+const CACHE_NAME = 'ies-mododios-v10.10'; // Actualiza la versión para forzar la activación
 const urlsToCache = [
   './index.html',
   './manifest.json',
